18c8e138e3a237d5da36c98a8f689c96e7a4fee6
ashwin <ashwingatla@gmail.com>
2023-08-24 09:22:06 +0530

    Python scripts and documentation added

---
+++ Scripts/Python/conditions.py
@@ -0,0 +1,22 @@
+age = 18
+if age > 18:
+    print("Eligible for vote")
+
+
+if age > 18:
+    print("Eligigle for vote")
+else:
+    print("Under Aged")
+
+if age > 16:
+    if age >= 18:
+        print("Wait for another year")
+    else:
+        print("Eligigle for vote")
+
+if age < 14:
+    print ("You are a teenager")
+elif age < 18:
+    print ("Wait One more Year to Vote")
+else:
+    print ("Eligigle for vote")
